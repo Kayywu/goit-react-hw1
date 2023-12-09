570b2707f8037a59deb71e0718671b00a1aedef3
Kayywu <katherine.wosik@gmail.com>
2023-12-09 20:57:08 +0100

    id-friendlistitem

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -19,6 +19,7 @@ export const FriendListItem = ({ avatar, name, isOnline }) => {
 };
 
 FriendListItem.propTypes = {
+    id: PropTypes.number,
     avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     isOnline: PropTypes.bool.isRequired,
